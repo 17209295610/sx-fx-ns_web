1a84f5baee92a6de0910df2f0411362073c854b1
逸墨 <2798156978@qq.com>
2025-02-06 19:35:06 +0800

    fix: 修复路由和部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   optimizeDeps: {
     include: ['vue', 'vue-router', '@vueuse/core']
   },
-  base: '/fengxiang-nisu-web/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
